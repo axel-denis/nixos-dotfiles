50f19aed3b2261d0bebc7920150326bb918851ce
Axel Denis <91873203+axel-denis@users.noreply.github.com>
2025-03-23 13:16:48 +0900

    Update packages.nix
    added firefox

--- laptop/packages.nix
+++ laptop/packages.nix
@@ -1,12 +1,55 @@
 { config, pkgs, inputs, ... }:
 {
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Bootloader.
+#  boot.loader.grub.enable = pkgs.lib.mkForce true;
+#  boot.loader.grub.device = "/dev/vda";
+#  boot.loader.grub.useOSProber = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  time.timeZone = "Asia/Seoul";
+
+  networking.networkmanager.enable = true;
+  programs.hyprland.enable = true;
+  services.displayManager.defaultSession = "hyprland";
+  services.fail2ban.enable = true; # block repeated ssh login attemps
+  services.tlp.enable = true; # power gestion/savings
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ALL = "en_US.UTF-8";
+    LC_CTYPE = "en_US.UTF8";
+    LC_ADDRESS = "es_VE.UTF-8";
+    LC_IDENTIFICATION = "es_VE.UTF-8";
+    LC_MEASUREMENT = "es_VE.UTF-8";
+    LC_MESSAGES = "en_US.UTF-8";
+    LC_MONETARY = "es_VE.UTF-8";
+    LC_NAME = "es_VE.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "es_VE.UTF-8";
+    LC_TELEPHONE = "es_VE.UTF-8";
+    LC_TIME = "es_VE.UTF-8";
+    LC_COLLATE = "es_VE.UTF-8";
+  };
+
+➜  ~ cat /etc/nixos/packages.nix 
+{ config, pkgs, inputs, ... }:
+{
   environment.systemPackages = with pkgs; [
     neofetch
     pkgs.kitty # required for the default Hyprland config
     rsync # for dotfiles copy
 
     wofi
-    vscode    
+    vscode
+    firefox
     brightnessctl
     pipewire
     wireplumber # check if the two are necessary
