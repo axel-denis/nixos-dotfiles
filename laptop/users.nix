0810b9e3756e76c6ce7e5403e9f3830fdf47feda
Axel Denis <91873203+axel-denis@users.noreply.github.com>
2025-03-23 13:17:41 +0900

    Update users.nix

--- laptop/users.nix
+++ laptop/users.nix
@@ -5,6 +5,6 @@
     description = "Axel";
     extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [];
-    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM6TnZanVTSOFIoGj7CxP7MygdM9G9Pxzm7FgqbMnxi9 axel@rarch" ];
+    #openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM6TnZanVTSOFIoGj7CxP7MygdM9G9Pxzm7FgqbMnxi9 axel@rarch" ];
   };
 }
