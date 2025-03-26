782de3f6efd08598b8925709544d2b13168f6f0f
Axel Denis <91873203+axel-denis@users.noreply.github.com>
2025-03-26 10:50:32 +0000

    feat: added arduino

--- laptop/packages.nix
+++ laptop/packages.nix
@@ -2,8 +2,20 @@
 {
   environment.systemPackages = with pkgs; [
     bitwarden-desktop
-    arduino
+    arduino-ide
     cargo
     nodejs_23
+  ];
+
+  # for arduino ide to compile to renesas board
+  services.udev.packages = [
+    (pkgs.writeTextFile {
+      name = "arduino_renesas_core_rules";
+      text = ''
+        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2341", MODE:="0666"
+      '';
+
+      destination = "/etc/udev/rules.d/60-arduino-renesas.rules";
+    })
   ];
 }
