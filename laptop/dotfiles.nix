a8fba58810335137cc7c6270fc10df84c79722bc
Axel Denis <axel.denis@epitech.eu>
2025-09-02 20:15:00 +0200

    feat: updated laptop dotfiles

--- laptop/dotfiles.nix
+++ laptop/dotfiles.nix
@@ -5,29 +5,32 @@ let
 
   dotfilesRepo = builtins.fetchGit {
     url = dotfilesRepoUrl;
-    rev = "0654fe10aa13f1ad63a254157a03356b0b32d8fa";
+    rev = "967d861c2cc03672e2f926f0abd93578f6de0ee0";
   };
 
   # Get all users with home directories
-  usersWithHome = pkgs.lib.filterAttrs (name: user: ((user.home != null) && (pkgs.lib.hasPrefix "/home/" user.home))) config.users.users;
+  usersWithHome = pkgs.lib.filterAttrs (name: user:
+    ((user.home != null) && (pkgs.lib.hasPrefix "/home/" user.home)))
+    config.users.users;
 
 in {
   # copying dotfiles configs to /home/$user/.config
   system.activationScripts.copyDotfiles = {
-    text = pkgs.lib.concatStringsSep "\n" (pkgs.lib.mapAttrsToList (username: user: ''
-      echo "Copying dotfiles to ${username}'s .config..."
-      config_dir="${user.home}/.config"
-      
-      # Create .config directory if it doesn't exist
-      ${pkgs.coreutils}/bin/mkdir -p "$config_dir"
-      
-      # Copy contents with rsync, preserving permissions
-      #${pkgs.rsync}/bin/rsync -rlpt --chown=${username}:users --delete \
-      #  ${dotfilesRepo}/ "$config_dir/"
-      ${pkgs.coreutils}/bin/cp -rf "${dotfilesRepo}/." "$config_dir/"
-      ${pkgs.coreutils}/bin/chown -R "$username" "$config_dir"
-      ${pkgs.coreutils}/bin/chmod +x "$config_dir/hypr/focus.sh"
-    '') usersWithHome);
+    text = pkgs.lib.concatStringsSep "\n" (pkgs.lib.mapAttrsToList
+      (username: user: ''
+        echo "Copying dotfiles to ${username}'s .config..."
+        config_dir="${user.home}/.config"
+
+        # Create .config directory if it doesn't exist
+        ${pkgs.coreutils}/bin/mkdir -p "$config_dir"
+
+        # Copy contents with rsync, preserving permissions
+        #${pkgs.rsync}/bin/rsync -rlpt --chown=${username}:users --delete \
+        #  ${dotfilesRepo}/ "$config_dir/"
+        ${pkgs.coreutils}/bin/cp -rf "${dotfilesRepo}/." "$config_dir/"
+        ${pkgs.coreutils}/bin/chown -R "$username" "$config_dir"
+        ${pkgs.coreutils}/bin/chmod +x "$config_dir/hypr/focus.sh"
+      '') usersWithHome);
 
     # The activation script should run after filesystems are mounted
     deps = [ "users" "groups" ];
